6f90ddbd40fbbf536054f56896990c1cf969609c
senia1 <oks314@gmail.com>
2023-09-08 15:49:01 +0300

    added ConnectionString, data context in Program.cs, NuGet package Tools

--- CustomersMVC/Program.cs
+++ CustomersMVC/Program.cs
@@ -1,10 +1,15 @@
 using BusinessLogicLayer;
+using DataAccessLayer.Data;
 using DataAccessLayer.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
