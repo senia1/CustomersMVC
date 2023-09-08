fc33e6ba2822bb73581bb75a8c7292a03d9f1a0c
senia1 <oks314@gmail.com>
2023-09-08 14:28:21 +0300

    added classes Customer, ApplicationDbContext, CustomerRepository and interface ICustomerRepository

---
+++ DataAccessLayer/Interfaces/CustomerRepository.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interfaces;
+
+public class CustomerRepository : ICustomerRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public CustomerRepository(ApplicationDbContext dbContext) 
+    { 
+        _dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<Customer>> GetCustomersAsync() 
+    {
+        return await _dbContext.Customers.ToListAsync();
+    }
+}
