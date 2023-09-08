63dea437dcd88070c21d4ec449c7ab7a2982a807
senia1 <oks314@gmail.com>
2023-09-08 14:58:55 +0300

    added CustomerController

---
+++ CustomersMVC/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogicLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomersMVC.Controllers;
+
+public class CustomerController : Controller
+{
+    private readonly ICustomerService _customerService;
+
+    public CustomerController(ICustomerService customerService)
+    {
+        _customerService = customerService;  
+    }
+    public async Task<IActionResult> Index()
+    {
+        var customers = await _customerService.GetCustomersAsync();
+        return View(customers);
+    }
+}
