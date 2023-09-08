98ee67c429fd0a84be0f086fe139e0815c47e21a
senia1 <oks314@gmail.com>
2023-09-08 14:40:22 +0300

    added classes in BusinessLogicLayer

---
+++ BusinessLogicLayer/CustomerService.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.Entities;
+using DataAccessLayer.Interfaces;
+
+namespace BusinessLogicLayer
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<IEnumerable<Customer>> GetCustomersAsync()
+        {
+            return await _customerRepository.GetCustomersAsync();
+        }
+    }
+}
