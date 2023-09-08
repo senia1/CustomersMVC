f28799307255bc8824c429a78554034e8a067fd8
senia1 <oks314@gmail.com>
2023-09-08 12:25:04 +0300

    Add project files.

---
+++ CustomersMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CustomersMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
